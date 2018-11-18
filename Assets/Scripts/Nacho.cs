50adaee10f944a5e93e3b24edbbc46aee7e3cc4a
mn-sngwn <mmsw0324@naver.com>
2018-11-19 03:57:19 +0900

    드래그 이벤트 추가 / 로직 생성중

--- Assets/Scripts/Nacho.cs
+++ Assets/Scripts/Nacho.cs
@@ -9,14 +9,20 @@ public class Nacho : MonoBehaviour {
     public GameObject Effect;
 
     public string ClipName, NewName; // 클립이름 PuzzleManager에서 자동으로 설정함 
-    public int index; // PuzzleManager의 컬럼 번호 (ArrayList에서의 인덱스를 바로 찾아오기 위함)
+    public int col; // PuzzleManager의 컬럼 번호 (ArrayList에서의 인덱스를 바로 찾아오기 위함)
+    public int row; // PuzzleManager의 컬럼 번호 (ArrayList에서의 인덱스를 바로 찾아오기 위함)
     public bool isDead = false;
-    
+    public bool isSelected = false;
+    public bool isStart = false;
+    public bool isDiagonal = false;
+    public static bool isDragging = false;
+    public static bool shouldDeselect = false;
+
     PuzzleManager manager;
 
     // Use this for initialization
     void Start () {
-
+        
         anim = GetComponent<Animator>();
         //anim.Play(ClipName);
         // 하이어라키의 PuzzleManager를 가져온다.
@@ -27,12 +33,58 @@ public class Nacho : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButton(0))
         {
-            Debug.Log("@@");
+
         }
 	}
 
-    void OnMouseDown()
+    public void SelectNacho(){
+        if(!isSelected)
+        {
+            //PuzzleManager.recentNacho = this;
+            isSelected = true;
+            transform.Rotate(new Vector3(0, 0, 10.0f));
+            //Debug.Log(row + ", " + col);
+        }
+    }
+
+    public void DeselectNacho(){
+        if(isSelected){
+            isStart = false;
+            isSelected = false;
+            transform.Rotate(new Vector3(0, 0, -10.0f));
+        }
+    }
+
+    private void OnMouseEnter()
     {
-        Debug.Log(index);
+        if(isDragging && IsDiagonal())
+        {
+            PuzzleManager.recentNacho = this;
+            SelectNacho();
+        }
+    }
+
+    private bool IsDiagonal(){
+
+        PuzzleManager.recentNacho = this;
+
+        Debug.Log("(" + PuzzleManager.recentNacho.col + ", " + PuzzleManager.recentNacho.row + ")" + " " + this.col + ", " + this.row);
+        Debug.Log(PuzzleManager.recentNacho.col == this.col || PuzzleManager.recentNacho.row == this.row);
+
+        return PuzzleManager.recentNacho.col == this.col || PuzzleManager.recentNacho.row == this.row ? true : false;
+    }
+
+    private void OnMouseDown()
+    {
+        PuzzleManager.recentNacho = this;
+        isStart = true;
+        SelectNacho();
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("(" + PuzzleManager.recentNacho.row + ", " + PuzzleManager.recentNacho.col + ")" + " " + this.row + ", " + this.col);
+
+        shouldDeselect = true;
     }
 }
--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -9,28 +9,57 @@ public class PuzzleManager : MonoBehaviour
     public GameObject RNacho;
     public GameObject Empty;
     public GameObject Field;
-    public Canvas temp;
+    public static Nacho recentNacho;
+    public Canvas canvas;
 
-    public ArrayList[] Block = new ArrayList[8];
-    //public Transform Nacho;
+    public GameObject[,] Block = new GameObject[8, 9];
 
     void Start()
     {
-        initField();
+        InitField();
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        DetectDragging();
+        DetectDeselect();
+    }
 
+    void DetectDragging()
+    {
+
+        foreach (GameObject nacho in Block)
+        {
+            if (nacho.GetComponent<Nacho>().isSelected)
+            {
+                recentNacho = nacho.GetComponent<Nacho>();
+                Nacho.isDragging = true;
+            }
+        }
+    }
+
+    void DetectDeselect()
+    {
+
+        foreach (GameObject nacho in Block)
+        {
+            if (Nacho.shouldDeselect)
+            {
+                Nacho.isDragging = false;
+                nacho.GetComponent<Nacho>().DeselectNacho();
+            }
+        }
+
+        Nacho.shouldDeselect = false;
 
     }
 
-    void initField()
+    void InitField()
     {
         var ratio = (float)Screen.height / Screen.width;
-        temp = GameObject.Find("Canvas").GetComponent<Canvas>();
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
 
         if (ratio < 1.8F) ratio = 2.5F;
 
@@ -38,21 +67,18 @@ public class PuzzleManager : MonoBehaviour
         RNacho.transform.localScale = new Vector3(ratio / 50, ratio / 50, 0);
         Field.transform.localScale = new Vector3(ratio / 6.5F, ratio / 6.5F, 0);
 
+        Field.AddComponent<FieldController>();
+
         Instantiate(Field, new Vector3(0, 0, 1000), Quaternion.identity);
 
-        for (int i = 0; i < 8; i++)
-        {
-            Block[i] = new ArrayList();
-        }
-
-        GameObject nacho;
+        GameObject nacho = null;
         float x_space = 0, y_space = 0, x_start = 0, y_start = 0;
 
-        for (int y = 0; y < 8; y++)
+        for (int row = 0; row < 8; row++)
         {
-            for (int x = 0; x < 9; x++)
+            for (int col = 0; col < 9; col++)
             {
-                switch (y)
+                switch (row)
                 {
                     case 0:
                         x_start = -0.8161F;
@@ -60,23 +86,15 @@ public class PuzzleManager : MonoBehaviour
                         x_space = 0.2028F;
                         y_space = 0.3431F;
 
-                        if (x == 2 || x == 4 || x == 6) { nacho = YNacho; }
+                        if (col == 2 || col == 4 || col == 6) { nacho = YNacho; }
                         else { nacho = Empty; }
-
-                        Block[y].Add(
-                            CreateNacho(
-                                nacho, x,
-                                new Vector3(
-                                    ratio * x_start + (x * ratio * x_space),
-                                    ratio * y_start - (y * ratio * y_space),
-                                    1000)));
                         break;
 
                     case 1:
                     case 3:
                     case 5:
 
-                        if (x % 2 == 0)
+                        if (col % 2 == 0)
                         {
                             x_start = -0.8161F;
                             y_start = 1.2231F;
@@ -92,20 +110,12 @@ public class PuzzleManager : MonoBehaviour
                             y_space = 0.3431F;
                             nacho = YNacho;
                         }
-
-                        Block[y].Add(
-                            CreateNacho(
-                                nacho, x,
-                                new Vector3(
-                                    ratio * x_start + (x * ratio * x_space),
-                                    ratio * y_start - (y * ratio * y_space),
-                                    1000)));
                         break;
 
                     case 2:
                     case 4:
                     case 6:
-                        if (x % 2 == 0)
+                        if (col % 2 == 0)
                         {
                             x_start = -0.8161F;
                             y_start = 1.1778F;
@@ -122,13 +132,6 @@ public class PuzzleManager : MonoBehaviour
                             nacho = RNacho;
                         }
 
-                        Block[y].Add(
-                            CreateNacho(
-                                nacho, x,
-                                new Vector3(
-                                    ratio * x_start + (x * ratio * x_space),
-                                    ratio * y_start - (y * ratio * y_space),
-                                    1000)));
                         break;
 
                     case 7:
@@ -137,29 +140,27 @@ public class PuzzleManager : MonoBehaviour
                         x_space = 0.2028F;
                         y_space = 0.3431F;
 
-                        if (x == 2 || x == 4 || x == 6) { nacho = RNacho; }
+                        if (col == 2 || col == 4 || col == 6) { nacho = RNacho; }
                         else { nacho = Empty; }
 
-                        Block[y].Add(
-                            CreateNacho(
-                                nacho, x,
-                                new Vector3(
-                                    ratio * x_start + (x * ratio * x_space),
-                                    ratio * y_start - (y * ratio * y_space),
-                                    1000)));
                         break;
 
                     default:
                         break;
                 }
+
+                Block[row, col] = CreateNacho(
+                        nacho, row, col,
+                        new Vector3(
+                            ratio * x_start + (col * ratio * x_space),
+                            ratio * y_start - (row * ratio * y_space), 1000));
             }
 
         }
     }
 
-    public GameObject CreateNacho(GameObject nacho, int idx, Vector3 pos) // idx는 컬럼의 인덱스 번호이고, pos는 실제 생성 위치
+    public GameObject CreateNacho(GameObject nacho, int row, int col, Vector3 pos) // idx는 컬럼의 인덱스 번호이고, pos는 실제 생성 위치
     {
-        //GameObject temp = Instantiate(nacho, pos, Quaternion.identity) as GameObject;
         GameObject temp = Instantiate(nacho) as GameObject;
         //temp.transform.SetParent(transform); // 생성된 캐릭터를 PuzzleManager의 자식으로 넣는다.
 
@@ -167,8 +168,9 @@ public class PuzzleManager : MonoBehaviour
         //temp.GetComponent<Nacho>().ClipName = string.Format("char{0:00}", 1);
         temp.transform.localPosition = pos;
         temp.name = nacho.ToString();
-        temp.GetComponent<Nacho>().index = idx;
-        
+        temp.GetComponent<Nacho>().col = col;
+        temp.GetComponent<Nacho>().row = row;
+
         return temp;
     }
     
